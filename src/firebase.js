21a00a6047c3790eb6a7a22a091b8f57cf7ae5fb
TracyMarieAndrea <102465656+TracyMarieAndrea@users.noreply.github.com>
2023-08-14 09:11:41 +0000

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyC-JvB2RXrsup4P9oH8IgCwUm1W5Ce08dQ",
-    authDomain: "tricyclebooking-ebe95.firebaseapp.com",
-    databaseURL: "https://tricyclebooking-ebe95-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "tricyclebooking-ebe95",
-    storageBucket: "tricyclebooking-ebe95.appspot.com",
-    messagingSenderId: "211757260511",
-    appId: "1:211757260511:web:08ab4ab453e1eb6c81d36a"
-  };  
-  
+  apiKey: "AIzaSyC-JvB2RXrsup4P9oH8IgCwUm1W5Ce08dQ",
+  authDomain: "tricyclebooking-ebe95.firebaseapp.com",
+  databaseURL: "https://tricyclebooking-ebe95-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tricyclebooking-ebe95",
+  storageBucket: "tricyclebooking-ebe95.appspot.com",
+  messagingSenderId: "211757260511",
+  appId: "1:211757260511:web:08ab4ab453e1eb6c81d36a"
+};  
+
   firebase.initializeApp(firebaseConfig);
   
   function form() {
@@ -17,12 +17,12 @@ const firebaseConfig = {
     const motorTypeV = document.getElementById("mt").value;
     const qrcodeV = document.getElementById("qrc").value;
     console.log(fullnameV, franchiseNumberV, driverRateV, motorTypeV, qrcodeV);
-  }
+};
+
+document.getElementById("submit-btn").onclick = function () {
+    form();
   
-  document.getElementById("submit-btn").onclick = function () {
-      form();
-    
-      firebase
+    firebase
       .database()
       .ref("drivers/" + driverUid)
       .set({
@@ -32,13 +32,12 @@ const firebaseConfig = {
         motorType: motorTypeV,
         qrcode: qrcodeV
       });
-  
-      alert("Data Inserted");
-      document.getElementById("name").value = "";
-      document.getElementById("fn").value = "";
-      document.getElementById("dr").value = "";
-      document.getElementById("mt").value = "";
-      document.getElementById("qrc").value = "";
-      
-    };
-  
+
+    alert("Data Inserted");
+    document.getElementById("name").value = "";
+    document.getElementById("fn").value = "";
+    document.getElementById("dr").value = "";
+    document.getElementById("mt").value = "";
+    document.getElementById("qrc").value = "";
+    
+  };
