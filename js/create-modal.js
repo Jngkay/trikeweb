a53e5a2467261602069b977cbf916cdad88f80fb
TracyMarieAndrea <102465656+TracyMarieAndrea@users.noreply.github.com>
2023-08-14 02:18:06 +0000

    create-modal

---
+++ js/create-modal.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    // Get the modal and the buttons
+    var modal = $(".modal-wrapper");
+    var openModalBtn = $("#addDriverBtn");
+    var closeModalBtn = $("#closeModalBtn");
+    var cancelBtn = $("#cancel-btn");
+  
+    // Open the modal
+    openModalBtn.click(function() {
+      modal.show();
+    });
+  
+    // Close the modal
+    closeModalBtn.click(function() {
+      modal.hide();
+    });
+
+    cancelBtn.click(function(){
+        modal.hide();
+    });
+  
+    // Close the modal if the user clicks outside of it
+    $(window).click(function(event) {
+      if (event.target === modal[0]) {
+        modal.hide();
+      }
+    });
+  });
+
+  
+
+  /*Firebase Function */
+
+
+  
