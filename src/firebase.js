66e9dffc4c2b6e2ca91b84e6ac9c0e944991f659
niquejoe <nikie.deocampo@gmail.com>
2023-09-08 01:36:32 +0000

    add submit effects

--- src/firebase.js
+++ src/firebase.js
@@ -14,6 +14,7 @@ const firebaseConfig = {
   /*Create Method */
   $("#submit-btn").click(function(e){
     e.preventDefault();
+    document.getElementById("lightBox-loader").style.display = "block";
 
     const fullname = $("#fullname").val();
     const email = $("#email").val();
@@ -77,7 +78,7 @@ const firebaseConfig = {
 
       });
     })
-    
+    document.getElementById("lightBox-loader").style.display = "none";
 });
 
 /*Drivers Read Method*/
