3b35c42ea1c6e1f86263c051c5972e7f6aafad6c
niquejoe <nikie.deocampo@gmail.com>
2023-09-08 05:36:28 +0000

    add info

--- jquery/create-modal.js
+++ jquery/create-modal.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
       modal.show();
     });
 
-    $(document).on('click', '.viewQR', function() { qrmodal.show(); qrmodal.attr( "data-info", "Nik");});
+    $(document).on('click', '.viewQR', function() { qrmodal.show(); qrmodal.attr( "data-info", this.data("info"));});
     $(document).on('click', '#viewQRClose', function() { qrmodal.hide(); });
   
     // Close the modal
